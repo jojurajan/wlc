8d08b767d6d1be3c239952ab5e092818e0c7b436
Joju Rajan <jojurajan@gmail.com>
2020-12-23 18:28:58 +0530

    Fixed issue with filename for Windows compatibility

--- wlc/test_base.py
+++ wlc/test_base.py
@@ -73,6 +73,7 @@ class ResponseHandler:
             .replace(" ", "-")
             .replace("[", "-")
             .replace("]", "-")
+            .replace("*", "-")
         )
         return body
 
